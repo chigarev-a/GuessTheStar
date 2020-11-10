4b8c0eac535860af1c9ad8cf0ea362a45949ea95
chigarev-a <a.chigarev96@gmail.com>
2020-11-10 21:35:19 +0500

    First commit

---
+++ app/src/main/java/com/example/guessthestar/MainActivity.java
@@ -0,0 +1,168 @@
+package com.example.guessthestar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button0;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private ImageView imageViewStar;
+
+    private String url = "https://www.forbes.ru/rating/403469-40-samyh-uspeshnyh-zvezd-rossii-do-40-let-reyting-forbes";
+    private ArrayList<String> names;
+    private ArrayList<String> urlsImg;
+
+    private int numberOfQuestion;
+    private int numberOfRightAnswer;
+    private ArrayList<Button> buttons;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        buttons = new ArrayList<>();
+        buttons.add(button0);
+        buttons.add(button1);
+        buttons.add(button2);
+        buttons.add(button3);
+        imageViewStar = findViewById(R.id.imageViewStar);
+
+        names = new ArrayList<>();
+        urlsImg = new ArrayList<>();
+
+        getContent();
+        playGame();
+    }
+
+
+    private void playGame(){
+        generateQuestions();
+        DownloadImageTask task = new DownloadImageTask();
+        try {
+            Bitmap bitmap = task.execute(urlsImg.get(numberOfQuestion)).get();
+            if(bitmap != null){
+                imageViewStar.setImageBitmap(bitmap);
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void generateQuestions(){
+        numberOfQuestion = (int) (Math.random() * names.size());
+        numberOfRightAnswer = (int) (Math.random() * buttons.size());
+
+    }
+//    Получение массивов имен и изображений
+    private void getContent(){
+        DownloadContentTask task = new DownloadContentTask();
+        try {
+            String content = task.execute(url).get();
+            String contentSplit = " ";
+            String start = "<table class=\"common_rating_list\">";
+            String end = "</table>";
+            Pattern patternSplitContent = Pattern.compile(start + "(.*?)" + end);
+            Matcher matcherSplitContent = patternSplitContent.matcher(content);
+            while(matcherSplitContent.find()){
+                contentSplit = matcherSplitContent.group(1);
+            }
+            Pattern patternImage = Pattern.compile("<img src=\"(.*?)\"");
+            Matcher matcherImage = patternImage.matcher(contentSplit);
+            while (matcherImage.find()){
+                urlsImg.add(matcherImage.group(1));
+            }
+            Pattern patternName = Pattern.compile("alt=\"(.*?)\"");
+            Matcher matcherName = patternName.matcher(contentSplit);
+            while (matcherName.find()){
+                names.add(matcherName.group(1));
+            }
+            for (String img : names){
+                Log.d("tag1", img);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    Загрузка контента страницы
+    private static class DownloadContentTask extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder content = new StringBuilder();
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while(line != null){
+                    content.append(line);
+                    line = reader.readLine();
+                }
+                return content.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(urlConnection != null){
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+    }
+
+//    Загрузка изображений
+    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap>{
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(urlConnection != null){
+                    urlConnection.disconnect();
+                }
+            }
+
+            return null;
+        }
+    }
+}
